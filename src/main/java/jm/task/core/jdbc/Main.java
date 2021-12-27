e54d8a6d51c1d9f0285187e661a675960cd133f7
krasnoperovamarina <krasnoperovamarina5@gmail.com>
2021-12-27 20:24:03 +0400

    test

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,21 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserService userService = new UserServiceImpl();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("Artem", "Artemov", (byte) 15);
+        userService.saveUser("Den", "Artemov", (byte) 36);
+        userService.saveUser("Oleg", "Ivanov", (byte) 26);
+        userService.removeUserById(1);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,21 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/mydatabase";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "427ivan";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+    }
+        return connection;
+    }
+}
